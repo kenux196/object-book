da025e9d83c6238c8fabb71187c600448d73521f
skyun <skyun@a2tec.co.kr>
2022-03-05 21:40:04 +0900

    object chapter 6

--- src/org/kenux/chapter06/eventloop/EventLoopApplication.java
+++ src/org/kenux/chapter06/eventloop/EventLoopApplication.java
@@ -1,13 +1,26 @@
 package org.kenux.chapter06.eventloop;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class EventLoopApplication {
     public static void main(String[] args) {
-        Event meeting = new Event(
+        Event event = new Event(
                 "회의",
                 LocalDateTime.of(2022, 3, 10, 10, 20),
                 Duration.ofMinutes(30));
+
+
+        RecurringSchedule schedule = new RecurringSchedule(
+                "회의",
+                DayOfWeek.WEDNESDAY,
+                LocalTime.of(10, 30),
+                Duration.ofMinutes(30));
+
+        if (!event.isSatisfied(schedule)) {
+            event.reschedule(schedule);
+        }
     }
 }
